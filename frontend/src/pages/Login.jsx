0fb2ac44395861000ad39c79a7ace9b8a38ac4e3
Abhhaaay <Abhay.Dwivedi2@amdocs.com>
2024-08-07 15:47:05 +0530

    created login and signup UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,53 @@
+import { Box, Button, FormControl, FormLabel, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import backgroundImg from '../assets/background.jpg';
+import { Link } from "react-router-dom";
+
 function Login() {
+  const [nameError, setNameError] = useState('');
+  const [passError, setPassError] = useState('');
+
+  const backgroundStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    backgroundImage: `url(${backgroundImg})`,
+    filter: "blur(8px)",
+    WebkitFilter: "blur(8px)",
+    zIndex: 0, 
+  };
+
+  const inputStyle = {
+    marginBottom: "1rem"
+  };
+
+  const formStyle = {
+    border: "0.2rem solid #ffffff",
+    padding: "3rem 1rem",
+    borderRadius: "1rem",
+    backgroundColor: "rgba(255, 255, 255, 0.479)",
+    position: "relative",
+    zIndex: 1,
+    width: "25rem",
+  };
+
   return (
-    <div>Login</div>
-  )
+    <Box style={{ position: "relative", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div style={backgroundStyle} />
+      <Box style={formStyle}>
+        <FormControl fullWidth>
+          <TextField style={inputStyle} variant="outlined" label="Username" error={!!nameError} helperText={nameError} />
+          <TextField style={inputStyle} variant="outlined" label="Password" type="password" error={!!passError} helperText={passError} />
+          <Button variant="contained"  sx={{textTransform: "none", marginTop: '0.2rem'}}>Log In</Button>
+        </FormControl>
+        <Typography sx={{textAlign: "center",marginTop: "0.4rem",color: "rgb(66, 66, 66)"}}>New User?  
+          <Link to="/sign-up">SignUp</Link>
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,50 @@
-
+import { Box, Button, FormControl, FormLabel, TextField } from "@mui/material";
+import { useState } from "react";
+import backgroundImg from '../assets/background.jpg';
 
 function Signup() {
+  const [nameError, setNameError] = useState('');
+  const [passError, setPassError] = useState('');
+
+  const backgroundStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    backgroundImage: `url(${backgroundImg})`,
+    filter: "blur(8px)",
+    WebkitFilter: "blur(8px)",
+    zIndex: 0, 
+  };
+
+  const inputStyle = {
+    marginBottom: "1rem"
+  };
+
+  const formStyle = {
+    border: "0.2rem solid #ffffff",
+    padding: "3rem 1rem",
+    borderRadius: "1rem",
+    backgroundColor: "rgba(255, 255, 255, 0.479)",
+    position: "relative",
+    zIndex: 1,
+    width: "25rem",
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <Box style={{ position: "relative", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div style={backgroundStyle} />
+      <Box style={formStyle}>
+        <FormControl fullWidth >
+          <TextField style={inputStyle} variant="outlined" label="Username" error={!!nameError} helperText={nameError} />
+          <TextField style={inputStyle} variant="outlined" label="Password" type="password" error={!!passError} helperText={passError} />
+          <TextField style={inputStyle} variant="outlined" label="Confirm Password" type="password" error={!!passError} helperText={passError} />
+          <Button variant="contained" sx={{textTransform: "none",  marginTop: '0.2rem'}}>Register</Button>
+        </FormControl>
+      </Box>
+    </Box>
+  );
 }
 
-export default Signup
+export default Signup;
