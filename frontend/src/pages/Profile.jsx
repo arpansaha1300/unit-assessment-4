0e50560b15841e57f4e7e785d540773686bcc70c
anubhavsingh013 <anubhsin@amdocs.com>
2024-08-08 16:13:32 +0530

    id column updated

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,19 @@
-
-
+import axios from "axios";
+import { useEffect } from "react"
+const supplierid=4;
 function Profile() {
+  useEffect(()=>{
+    const supplierdata=async()=>{
+      const response=axios.get(`http://localhost:8080/api/suppliers/${supplierid}`);
+      console.log((await response).data);
+    }
+    supplierdata();
+  },[]);
   return (
-    <div>P</div>
+    <div>
+      Hii anubhav welcome to amdocs
+      Profile page
+    </div>
   )
 }
 
