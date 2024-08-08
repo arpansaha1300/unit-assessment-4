ba0b1ebd3bdce385f52bbbb56e146bdc29190f4a
Arijit Debnath <arijitde@amdocs.com>
2024-08-08 14:15:15 +0530

    Updated Supplierpost api

--- backend/src/main/java/io/assessment/inventory/controller/SupplierController.java
+++ backend/src/main/java/io/assessment/inventory/controller/SupplierController.java
@@ -26,6 +26,7 @@ public class SupplierController {
         supplier.setName(supplierdto.getName());
         supplier.setEmail(supplierdto.getEmail());
         supplier.setPassword(supplierdto.getPassword());
+        supplier.setContactInfo(supplierdto.getContactInfo());
         return supplierService.saveSupplier(supplier);
     }
 
