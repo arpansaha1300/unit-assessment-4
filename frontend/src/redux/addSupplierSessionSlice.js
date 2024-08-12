506116cf501a305a21192b43a14cc41ccf540836
Arpan Saha <Arpan.Saha3@amdocs.com>
2024-08-12 12:20:57 +0530

    store add-supplier and add-package sessions (#14)

---
+++ frontend/src/redux/addSupplierSessionSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  name: "",
+  email: "",
+  contactInfo: "",
+};
+
+const addSupplierSessionSlice = createSlice({
+  name: "add-supplier",
+  initialState,
+  reducerPath: "add-supplier",
+  reducers: {
+    updateAddSupplierSessionName: (state, action) => {
+      state.name = action.payload;
+    },
+    updateAddSupplierSessionEmail: (state, action) => {
+      state.email = action.payload;
+    },
+    updateAddSupplierSessionContact: (state, action) => {
+      state.contactInfo = action.payload;
+    },
+    removeAddSupplierSession: (state) => {
+      state.name = "";
+      state.email = "";
+      state.contactInfo = "";
+    },
+  },
+});
+
+export const {
+  removeAddSupplierSession,
+  updateAddSupplierSessionContact,
+  updateAddSupplierSessionEmail,
+  updateAddSupplierSessionName,
+} = addSupplierSessionSlice.actions;
+
+export default addSupplierSessionSlice.reducer;
