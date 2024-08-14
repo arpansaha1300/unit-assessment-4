122c10c63c8042b5ed49ac6d7c3276449a5515e4
Arijit Debnath <arijitde@amdocs.com>
2024-08-14 10:39:21 +0530

    packages prices api

---
+++ backend/src/main/java/io/assessment/inventory/controller/dto/PriceRequestDto.java
@@ -0,0 +1,13 @@
+package io.assessment.inventory.controller.dto;
+
+public class PriceRequestDto {
+    private Long quantity;
+    
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+}
