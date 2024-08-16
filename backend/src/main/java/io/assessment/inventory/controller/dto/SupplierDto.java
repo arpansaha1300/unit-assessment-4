267c5dc19c16b01fa7067e12b6c7578d7afe0fb4
Arijit Debnath <arijitde@amdocs.com>
2024-08-16 16:12:26 +0530

    Updated apis

--- backend/src/main/java/io/assessment/inventory/controller/dto/SupplierDto.java
+++ backend/src/main/java/io/assessment/inventory/controller/dto/SupplierDto.java
@@ -6,38 +6,54 @@ public class SupplierDto {
     private String password;
     private String contactInfo;
     private String role;
+    private Double discount;
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getContactInfo() {
         return contactInfo;
     }
+
     public void setContactInfo(String contactInfo) {
         this.contactInfo = contactInfo;
     }
+
     public String getRole() {
         return role;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
-    
-    
 
-    
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
 }
